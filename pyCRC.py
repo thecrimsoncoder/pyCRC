42285a86fddb81febcb844b2dc508053f08238f8
Sean McElhare <sm860129@wcupa.edu>
2018-02-11 13:59:35 -0500

    Initial Commit

---
+++ pyCRC.py
@@ -0,0 +1,47 @@
+# pyCRC.py - Python3.6 based cyclic redundancy check
+print("+-------------------------------------+")
+print("|          p y C R C . P Y            |")
+print("| By: Sean McElhare (TheCrimsonCoder) |")
+print("| https://github.com/thecrimsoncoder  |")
+print("+-------------------------------------+")
+
+def main():
+
+    print("+-------------------------------------+")
+    print("| What would you like to do:          |")
+    print("| 1. Build CRC Code                   |")
+    print("| 2. Verify CRC Code                  |")
+    print("+-------------------------------------+")
+    option = int(input("OPTION: "))
+
+    if(option == 1):
+        data = int(input("Please input the data you would like to check: "))
+        key = int(input("Please input the CRC key you would like to use: "))
+        buildCRC(key,data)
+
+    elif(option == 2):
+        print("Placeholder for verifyCRC")
+
+def buildCRC(key,data):
+    print("Building CRC Code for: " + data + " using " + key)
+
+    # Number of bits that can be compared at a time
+    keyLength = len(key)
+    data = padZeros(data,)
+
+def padZeros (data,power):
+    print("Using CRC polynomial generator x^3 + x + 1")
+
+def checkValue(dataInt, keyInt):
+        if(dataInt == 0 and keyInt == 0):
+                return 0
+        elif(dataInt == 1 and keyInt == 0):
+                return 1
+        elif(dataInt == 0 and keyInt == 1):
+                return 1
+        elif(dataInt == 1 and keyInt == 1):
+                return 0
+        else:
+            print("Error (checkValue): An unexpected value was given (frameInt: " + dataInt + " generatorInt : " + keyInt + ")")
+
+main()
